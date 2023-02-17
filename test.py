0d14c0a449c4b664682c990316faaafe2b83d2b6
Kaushik Karthik Prasanna <kaushikkarthikp@gmail.com>
2023-02-17 18:06:51 -0500

    added some details, there is an error that needs to be fixed (multiple classes in a section are not registered)

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ soup = BeautifulSoup(content, "html.parser")
 initRan = False
 init = ""
 
-for subject in soup.find_all(class_="subject"):
+for subject in soup.find_all(class_="subject"): # for each subject
     # print the subject name
     subject_name = subject.find(class_="courseTitle").text.strip()
     if subject_name == init:
@@ -15,11 +15,22 @@ for subject in soup.find_all(class_="subject"):
     if not initRan:
         init = subject_name
         initRan = True
-    print(subject_name)
-    # for each subject print the section number and section index
-    # print the numeber of  divs in the sectionListings class
-    print(len(subject.find_all(class_="sectionData")))
+    print("\n", subject_name)
     
-    for section in subject.find_all(class_="sectionData"):
+    # print the number of sections in the each subject
+    # print(len(subject.find_all(class_="sectionData")))
+
+    for section in subject.find_all(class_="sectionData"): # for each section
         section_index = section.find(class_="sectionIndexNumber").text.strip()
         print("Section Index:", section_index)
+
+        # if sections contains the class "sectionMeetingTimesDiv" then print the details
+        for classes in section.find_all(class_="sectionMeetingTimesDiv"): # for each class
+            try: day = classes.find(class_="meetingTimeDay").text.strip()
+            except:
+                print("No class times available")
+                continue
+            time = classes.find(class_="meetingTimeHours").text.strip()
+            campus = classes.find(class_="meetingTimeCampus").text.strip()
+            room_number = classes.find(class_="meetingTimeBuildingAndRoom").text.strip()
+            print("Day:", day, "Time:", time, "Campus:", campus, "Room Number:", room_number)
